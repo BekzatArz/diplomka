e69de6e31fe05578268a2e05dcf6b20a14953dae
Beksii <heliumvalley@gmail.com>
2026-04-20 15:37:49 +0600

    first commit

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -14,7 +14,14 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    CORS(app)
+    CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "http://localhost:3000",
+            "https://diplomka-umxh-ch7vqnzxs-bekzatarzs-projects.vercel.app"
+        ]
+    }
+})
 
     db.init_app(app)
     migrate.init_app(app, db)
